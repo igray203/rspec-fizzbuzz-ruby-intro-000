0fa4f1d2d59c99493c81ce1f399ac30921359695
ian gray <igrayscale@gmail.com>
2019-08-17 16:33:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz
+  if int % 3 == 0
+    puts "Fizz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  elsif int % 3 == 0 && int % 5 == 0
+    puts "FizzBuzz"
+  elsif int % 3 =/= 0 && int % 5 =/= 0
+    return 'nil'
 
 end
